e4dd9c8acc888716fdd4c7048ad9b8663df72137
Adithyaav123 <93602832+Adithyaav123@users.noreply.github.com>
2023-10-05 14:38:18 +0530

    Facade app(console)file

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using FacadeDPClassLibrary;
+namespace FacadeDPApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HotelReception hotelReception = new HotelReception();
+
+            Console.WriteLine("**************Getting Indian Menu:**************");
+            hotelReception.GetIndianMenu();
+
+            Console.WriteLine("**************Getting Italian Menu:**************");
+            hotelReception.GetItalianMenu();
+
+            Console.WriteLine("**************Getting Chinese Menu:**************");
+            hotelReception.GetChineseMenu();
+
+            Console.ReadLine();
+        }
+    }
+}
